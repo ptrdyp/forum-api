6fb88a5e62bcd47f7f4973ca585a75e67bd952c5
ptrdyp <alviany.prameswari@gmail.com>
2025-06-15 13:18:34 +0700

    pass setCommentLikeUseCase test

--- src/Applications/use_case/SetCommentLikesUseCase.js
+++ src/Applications/use_case/SetCommentLikesUseCase.js
@@ -1,5 +1,16 @@
 class SetCommentLikesUseCase {
+  constructor({ commentRepository, likeRepository }) {
+    this._commentRepository = commentRepository;
+    this._likeRepository = likeRepository;
+  }
 
+  async execute(useCaseAuth, useCaseParam) {
+    const { id } = useCaseAuth;
+    const { threadId, commentId } = useCaseParam;
+
+    await this._commentRepository.verifyAvailableComment(threadId, commentId);
+    await this._likeRepository.setCommentLikes(id, commentId);
+  }
 }
 
-module.exports = SetCommentLikesUseCase
+module.exports = SetCommentLikesUseCase;
